ee805a6669fe9cf0b5419ad535341a84c988053a
kida7kahp@gmail.com <kida7kahp@gmail.com@076ae3c3-3940-0c6e-3dd9-7e518ec0454b>
2011-02-27 14:24:17 +0000

    Lần upload đầu tiên, thết kế xong bàn cờ và hàm check GameOver

---
+++ Caro/CaroBoard.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caro
+{
+    class Cell
+    {
+        public char state;
+        public Cell(char state)
+        {
+            this.state = state;
+        }
+    }
+    class CaroBoard
+    {
+        public Cell[,] cells;
+        public bool XPlaying;
+        public int size { get; private set; }
+        private int[] dx = { 0, 1, -1, 1 };
+        private int[] dy = { 1, 0, 1, 1 };
+        //public bool GameOver = false;
+        public bool IsGameOver
+        {
+            get
+            {
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        if (cells[i, j].state == 'x' || cells[i, j].state == 'o')
+                            if (Check5Row(i, j, 1, 4) >= 5)
+                                return true;
+                return false;
+            }
+        }
+        public int Check5Row(int x,int y,int count,int type)
+        {
+            if (type == 4)
+            {
+                int max=0;
+                for(int i=0;i<4;i++)
+                {
+                    int u=x+dx[i];
+                    int v=y+dy[i];
+                    if (u >= 0 && v >= 0 & u < size && v < size && cells[u, v].state == cells[x, y].state)
+                        max = Math.Max(max, Check5Row(u, v, count + 1, i));
+                }
+                return max;
+            }
+            else
+            {
+                int u = x + dx[type];
+                int v = y + dy[type];
+                if (u >= 0 && v >= 0 & u < size && v < size && cells[u, v].state == cells[x, y].state)
+                    return Check5Row(u, v, count + 1, type);
+                return count;
+            }
+        }
+        private void New(int n)
+        {
+            XPlaying = true;
+            size = n;
+            cells = new Cell[size, size];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    cells[i, j] = new Cell(' ');
+        }
+        public CaroBoard(int n)
+        {
+            New(n);
+        }
+        public CaroBoard():this(14)
+        {
+
+        }
+    }
+}
---
+++ Caro/CaroBoardUI.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Caro
+{
+    public partial class CaroBoardUI : UserControl
+    {
+        //public event EventHandler CellClick;
+#region properties
+        const int CELL_SIZE = 25;
+        Image _ImgX;
+        Image _ImgO;
+        CaroBoard _board;
+        bool GameOver = false;
+        bool StopTimer = false;
+#endregion
+
+        public CaroBoardUI()
+        {
+            InitializeComponent();
+            _ImgX = new Bitmap(Properties.Resources.x, CELL_SIZE, CELL_SIZE);
+            _ImgO = new Bitmap(Properties.Resources.o, CELL_SIZE, CELL_SIZE);
+            _board = new CaroBoard();
+            this.NewGame(true);
+        }
+        /// <summary>
+        /// Bắt đầu game mới
+        /// </summary>
+        /// <param name="defaultFirst">Bằng true thì người chơi đầu tiên sẽ là mặc định theo lớp CaroBoard</param>
+        public void NewGame(bool defaultFirst)
+        {
+            GameOver = false;
+            StopTimer = false;
+            _board = new CaroBoard(19);
+            if (!defaultFirst) _board.XPlaying = !_board.XPlaying;
+            this.MaximumSize = new Size(_board.size * CELL_SIZE + 4, _board.size * CELL_SIZE + 4);
+            this.MinimumSize = new Size(_board.size * CELL_SIZE + 4, _board.size * CELL_SIZE + 4);
+            this.Size = new Size(_board.size * CELL_SIZE, _board.size * CELL_SIZE);
+            Invalidate();
+            timer1.Start();
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (!GameOver)
+            {
+                int i = (int)(e.Y / CELL_SIZE);
+                int j = (int)(e.X / CELL_SIZE);
+                if (_board.cells[i, j].state == ' ')
+                {
+                    _board.cells[i, j].state = _board.XPlaying ? 'x' : 'o';
+                    _board.XPlaying = !_board.XPlaying;
+                    GameOver = _board.IsGameOver;
+                    
+                }
+                else _board.cells[i, j].state = ' ';
+                Rectangle rc = new Rectangle(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                Invalidate(rc);
+            }
+            base.OnMouseDown(e);
+        } 
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            int y = 0,x;
+            for (int i = 0; i < _board.size; i++)
+            {
+                x = 0;
+                for(int j=0;j<_board.size;j++)
+                {
+                    
+                    if (_board.cells[i, j].state == 'x')
+                        e.Graphics.DrawImage(_ImgX, x, y);
+                    else if (_board.cells[i, j].state == 'o')
+                        e.Graphics.DrawImage(_ImgO, x, y);
+                    e.Graphics.DrawLine(Pens.Black, 0, x, this.Height, x);
+                    x += CELL_SIZE;
+                }
+                e.Graphics.DrawLine(Pens.Black, y, 0, y, this.Width);
+                y += CELL_SIZE;
+            }
+            base.OnPaint(e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (StopTimer) return;
+            if(GameOver)
+            {
+                StopTimer = true;
+                if (_board.XPlaying) MessageBox.Show("Quân O thắng.");
+                else MessageBox.Show("Quân X thắng.");
+                timer1.Stop();
+            }
+        }
+    }
+}
